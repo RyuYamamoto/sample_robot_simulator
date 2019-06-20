6731880be28963304a4261c01ce4358e7e26f3e0
RyuYamamoto <ryu.yamamoto04@g.softbank.co.jp>
2019-06-21 03:55:13 +0900

    add teleop pkg

---
+++ sample_robot_teleop/src/sample_robot_teleop_node.cpp
@@ -0,0 +1,31 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
+
+geometry_msgs::Twist cmd_vel;
+
+void joy_callback(const sensor_msgs::Joy& joy_msg)
+{
+	cmd_vel.linear.x = joy_msg.axes[1];
+	cmd_vel.linear.y = joy_msg.axes[3];
+	cmd_vel.angular.z = joy_msg.axes[0];
+}
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "sample_robot_teleop_node");
+	ros::NodeHandle nh;
+
+	ros::Publisher cmd_pub = nh.advertise<geometry_msgs::Twist>("/sample_robot/diff_drive_controller/cmd_vel", 1000);
+
+	ros::Subscriber joy_sub = nh.subscribe("joy", 10, joy_callback);
+
+	ros::Rate rate(10);
+	while(ros::ok())
+	{
+		cmd_pub.publish(cmd_vel);
+		ros::spinOnce();
+		rate.sleep();
+	}
+	return 0;
+}
